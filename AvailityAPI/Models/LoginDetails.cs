57dc104b15ec1315ca29ab307da8dce6e2683900
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2021-10-27 16:33:04 -0400

    Add project files.

---
+++ AvailityAPI/Models/LoginDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvailityAPI.Models
+{
+    public class LoginDetails
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(256)")]
+        public string FirstName { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(256)")]
+        public string LastName { get; set; }
+        [Column(TypeName = "nvarchar(256)")]
+        public int NpiNumber { get; set; }
+        [MaxLength(15)]
+        public int PhoneNumber { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(256)")]
+        public string Email { get; set; }
+    }
+}
---
+++ AvailityAPI/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvailityAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "AvailityAPI", Version = "v1" } ));
+
+            // Cross Origin Resource Sharing: Is a W3C standard that allows a server to relax the same-origin policy.
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    // --> DEBUG
+                    var tmp = Configuration["WebClients:Availity"];
+                    // DEBUG <--
+
+                    policy.WithOrigins(Configuration[tmp])
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AvailityAPI v1"));
+            }
+
+            app.UseCors();
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
