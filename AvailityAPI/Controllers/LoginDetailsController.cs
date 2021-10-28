5f7d46fa3bd6f539e9ed61fc25212e3ec3d36003
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2021-10-27 21:36:19 -0400

    Code cleanup.

--- AvailityAPI/Controllers/LoginDetailsController.cs
+++ AvailityAPI/Controllers/LoginDetailsController.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using AvailityAPI.Domain.Entities;
+using AvailityAPI.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using AvailityAPI.Domain.Entities;
-using AvailityAPI.Infrastructure;
 
 namespace AvailityAPI.Controllers
 {
--- AvailityAPI/Startup.cs
+++ AvailityAPI/Startup.cs
@@ -1,18 +1,11 @@
 using AvailityAPI.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AvailityAPI
 {
@@ -30,9 +23,9 @@ namespace AvailityAPI
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo {Title = "AvailityAPI", Version = "v1"} ));
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "AvailityAPI", Version = "v1" }));
             services.AddDbContext<LoginDetailContext>(options =>
-                options.UseSqlServer( Configuration.GetConnectionString("Default") ));
+                options.UseSqlServer(Configuration.GetConnectionString("Default")));
 
             // Cross Origin Resource Sharing: Is a W3C standard that allows a server to relax the same-origin policy.
             services.AddCors(options =>
